6e7f5c33c5bab28367a4483f5525ce9e14021f00
Sarthak Sharma <sarthu858@gmail.com>
2022-11-06 19:31:03 +0530

    Data updated...

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,12 +213,12 @@
     /**
      * Preloader
      */
-    let preloader = select('#preloader');
-    if (preloader) {
-        window.addEventListener('load', () => {
-            preloader.remove()
-        });
-    }
+    // let preloader = select('#preloader');
+    // if (preloader) {
+    //     window.addEventListener('load', () => {
+    //         preloader.remove()
+    //     });
+    // }
 
 })()
 
